164fbd00d03417747d405d1079ecbe00d79dfd20
Anton Malofeev <arenukvern@gmail.com>
2023-01-22 04:24:34 +0200

    fix: error name

--- packages/ts_core/lib/src/analytics/firebase/firebase_web.dart
+++ packages/ts_core/lib/src/analytics/firebase/firebase_web.dart
@@ -60,7 +60,7 @@ class FirebaseAnalyticsPlugin implements AbstractAnalytics {
       printDetails: printDetails,
     );
     await analytics.logEvent(
-      name: 'error',
+      name: 'flutter_error',
       parameters: {'details': errorDetailsStr},
     );
   }
