3c6338123b0b9973d81a858a33bdc0a287183375
Anton Malofeev <arenukvern@gmail.com>
2023-02-04 04:35:10 +0200

    feat: clear & update & insert

--- packages/ts_core/test/data_services/table_syncer_service_test.dart
+++ packages/ts_core/test/data_services/table_syncer_service_test.dart
@@ -3,39 +3,86 @@ import 'package:ts_core/ts_core.dart';
 
 void main() {
   group('TableSyncerService', () {
-    test('can syncTables', () async {
+    final sourceTableParam = TableParamsModel(
+      id: '1',
+      workbookOriginName: '',
+      userId: '',
+      createdAt: DateTime.now(),
+      dataTopLeftCell: const CellModel(columnIndex: 20, rowIndex: 2),
+      headerTopLeftCell: const CellModel(columnIndex: 20, rowIndex: 3),
+      keyColumnName: 'id',
+    );
+    final destinationTableParam = TableParamsModel(
+      id: '2',
+      workbookOriginName: '',
+      userId: '',
+      createdAt: DateTime.now(),
+      dataTopLeftCell: const CellModel(columnIndex: 1, rowIndex: 1),
+      headerTopLeftCell: const CellModel(columnIndex: 1, rowIndex: 2),
+      keyColumnName: 'id',
+    );
+    final tablesMap = {
+      sourceTableParam.id: sourceTableParam,
+      destinationTableParam.id: destinationTableParam,
+    };
+
+    final mockTablesHeaders = {
+      sourceTableParam.headerTopLeftCell: [
+        ['id', 'color', 'name', 'brand', 'size']
+      ],
+      destinationTableParam.headerTopLeftCell: [
+        ['planet', 'solar system', 'size', 'color', 'id']
+      ]
+    };
+    final mockTables = {
+      sourceTableParam.dataTopLeftCell: [
+        ['1', 'white', 'shirt', 'chief', 'medium'],
+        ['3', 'grey', 'pants', 'parabens', 'medium'],
+        ['6', 'black', 'car', 'la', 'large'],
+        ['7', 'red', 'vagon', 'wbw', 'small'],
+      ],
+      destinationTableParam.dataTopLeftCell: [
+        ['earth', 'solar', 'x', '45', '3'],
+        ['venus', 'alpha centauri', 'd', '21', '5'],
+        ['mars', 'persei', 'c', '3', '6'],
+        ['sun', 'Zeta Reticuli', 's', '6', '4'],
+      ],
+    };
+    final expectedWithUpdatedTable = [
+      ['earth', 'solar', 'medium', 'grey', '3'],
+      ['venus', 'alpha centauri', 'd', '21', '5'],
+      ['mars', 'persei', 'large', 'black', '6'],
+      ['sun', 'Zeta Reticuli', 's', '6', '4'],
+    ];
+    final expectedWithUpdatedNewTable = [
+      ...expectedWithUpdatedTable,
+      [null, null, 'medium', 'white', '1'],
+      [null, null, 'small', 'red', '7'],
+    ];
+    final expectedWithUpdatedClearTable = [
+      ['earth', 'solar', 'medium', 'grey', '3'],
+      ['venus', 'alpha centauri', null, null, '5'],
+      ['mars', 'persei', 'large', 'black', '6'],
+      ['sun', 'Zeta Reticuli', null, null, '4'],
+    ];
+    final expectedWithInsertTable = [
+      ...mockTables[destinationTableParam.dataTopLeftCell]!,
+      // [null, null, 'medium', 'white', '1'],
+      // [null, null, 'small', 'red', '7'],
+      [null, null, null, null, '1'],
+      [null, null, null, null, '7'],
+    ];
+    test('can syncTables with shouldUpdateValues, shouldAddNewValues',
+        () async {
       // final apiServices = ApiServices.create(context);
       // final tablesNotifier = SyncParamsNotifier(apiServices: apiServices);
-
-      final sourceTableParam = TableParamsModel(
-        id: '1',
-        workbookOriginName: '',
-        userId: '',
-        createdAt: DateTime.now(),
-        dataTopLeftCell: const CellModel(columnIndex: 20, rowIndex: 2),
-        headerTopLeftCell: const CellModel(columnIndex: 20, rowIndex: 3),
-        keyColumnName: 'id',
-      );
-      final destinationTableParam = TableParamsModel(
-        id: '2',
-        workbookOriginName: '',
-        userId: '',
-        createdAt: DateTime.now(),
-        dataTopLeftCell: const CellModel(columnIndex: 1, rowIndex: 1),
-        headerTopLeftCell: const CellModel(columnIndex: 1, rowIndex: 2),
-        keyColumnName: 'id',
-      );
-      final tablesMap = {
-        sourceTableParam.id: sourceTableParam,
-        destinationTableParam.id: destinationTableParam,
-      };
       final syncParams = TablesSyncParamsModel(
         id: '',
         userId: '',
         createdAt: DateTime.now(),
         sourceTableId: sourceTableParam.id,
         destinationTablesIds: [destinationTableParam.id],
-        columnNames: const ['size', 'id', 'color'],
+        columnNames: const ['size', 'color'],
         shouldUpdateValues: true,
         shouldAddNewValues: true,
         shouldClearValueBeforeUpdate: false,
@@ -45,38 +92,6 @@ void main() {
         syncParams: syncParams,
         tablesMap: tablesMap,
       );
-
-      final mockTablesHeaders = {
-        sourceTableParam.headerTopLeftCell: [
-          ['id', 'color', 'name', 'brand', 'size']
-        ],
-        destinationTableParam.headerTopLeftCell: [
-          ['planet', 'solar system', 'size', 'color', 'id']
-        ]
-      };
-      final mockTables = {
-        sourceTableParam.dataTopLeftCell: [
-          ['1', 'white', 'shirt', 'chief', 'medium'],
-          ['3', 'grey', 'pants', 'parabens', 'medium'],
-          ['6', 'black', 'car', 'la', 'large'],
-          ['7', 'red', 'vagon', 'wbw', 'small'],
-        ],
-        destinationTableParam.dataTopLeftCell: [
-          ['earth', 'solar', 'x', '45', '3'],
-          ['venus', 'alpha centauri', 'd', '21', '5'],
-          ['mars', 'persei', 'c', '3', '6'],
-          ['sun', 'Zeta Reticuli', 's', '6', '4'],
-        ],
-      };
-      final expectedUpdatedTable = [
-        ['earth', 'solar', 'medium', 'grey', '3'],
-        ['venus', 'alpha centauri', 'd', '21', '5'],
-        ['mars', 'persei', 'large', 'black', '6'],
-        ['sun', 'Zeta Reticuli', 's', '6', '4'],
-        [null, null, 'medium', 'white', '1'],
-        [null, null, 'small', 'red', '7'],
-      ];
-
       final excelTableApi = ExcelTableMockApi(
         tables: {...mockTables, ...mockTablesHeaders},
       );
@@ -89,7 +104,108 @@ void main() {
       final updatedDestinationTable =
           excelTableApi.tables[destinationTableParam.dataTopLeftCell];
 
-      expect(updatedDestinationTable, equals(expectedUpdatedTable));
+      expect(updatedDestinationTable, equals(expectedWithUpdatedNewTable));
+    });
+    test('can syncTables with shouldUpdateValues', () async {
+      // final apiServices = ApiServices.create(context);
+      // final tablesNotifier = SyncParamsNotifier(apiServices: apiServices);
+      final syncParams = TablesSyncParamsModel(
+        id: '',
+        userId: '',
+        createdAt: DateTime.now(),
+        sourceTableId: sourceTableParam.id,
+        destinationTablesIds: [destinationTableParam.id],
+        columnNames: const ['size', 'color'],
+        shouldUpdateValues: true,
+        shouldAddNewValues: false,
+        shouldClearValueBeforeUpdate: false,
+      );
+
+      final runtimeSyncParams = SyncParamsNormalizer.normalize(
+        syncParams: syncParams,
+        tablesMap: tablesMap,
+      );
+      final excelTableApi = ExcelTableMockApi(
+        tables: {...mockTables, ...mockTablesHeaders},
+      );
+      final excelTableSyncerService = ExcelTableSyncerService(
+        excelTableApi: excelTableApi,
+      );
+      await excelTableSyncerService.syncTables(
+        runtimeSyncParams: runtimeSyncParams,
+      );
+      final updatedDestinationTable =
+          excelTableApi.tables[destinationTableParam.dataTopLeftCell];
+
+      expect(updatedDestinationTable, equals(expectedWithUpdatedTable));
+    });
+    test(
+        'can syncTables with shouldUpdateValues & shouldClearValueBeforeUpdate',
+        () async {
+      // final apiServices = ApiServices.create(context);
+      // final tablesNotifier = SyncParamsNotifier(apiServices: apiServices);
+      final syncParams = TablesSyncParamsModel(
+        id: '',
+        userId: '',
+        createdAt: DateTime.now(),
+        sourceTableId: sourceTableParam.id,
+        destinationTablesIds: [destinationTableParam.id],
+        columnNames: const ['size', 'color'],
+        shouldUpdateValues: true,
+        shouldAddNewValues: false,
+        shouldClearValueBeforeUpdate: true,
+      );
+
+      final runtimeSyncParams = SyncParamsNormalizer.normalize(
+        syncParams: syncParams,
+        tablesMap: tablesMap,
+      );
+      final excelTableApi = ExcelTableMockApi(
+        tables: {...mockTables, ...mockTablesHeaders},
+      );
+      final excelTableSyncerService = ExcelTableSyncerService(
+        excelTableApi: excelTableApi,
+      );
+      await excelTableSyncerService.syncTables(
+        runtimeSyncParams: runtimeSyncParams,
+      );
+      final updatedDestinationTable =
+          excelTableApi.tables[destinationTableParam.dataTopLeftCell];
+
+      expect(updatedDestinationTable, equals(expectedWithUpdatedClearTable));
+    });
+    test('can syncTables with shouldAddNewValues', () async {
+      // final apiServices = ApiServices.create(context);
+      // final tablesNotifier = SyncParamsNotifier(apiServices: apiServices);
+      final syncParams = TablesSyncParamsModel(
+        id: '',
+        userId: '',
+        createdAt: DateTime.now(),
+        sourceTableId: sourceTableParam.id,
+        destinationTablesIds: [destinationTableParam.id],
+        columnNames: const ['size', 'color'],
+        shouldUpdateValues: false,
+        shouldAddNewValues: true,
+        shouldClearValueBeforeUpdate: false,
+      );
+
+      final runtimeSyncParams = SyncParamsNormalizer.normalize(
+        syncParams: syncParams,
+        tablesMap: tablesMap,
+      );
+      final excelTableApi = ExcelTableMockApi(
+        tables: {...mockTables, ...mockTablesHeaders},
+      );
+      final excelTableSyncerService = ExcelTableSyncerService(
+        excelTableApi: excelTableApi,
+      );
+      await excelTableSyncerService.syncTables(
+        runtimeSyncParams: runtimeSyncParams,
+      );
+      final updatedDestinationTable =
+          excelTableApi.tables[destinationTableParam.dataTopLeftCell];
+
+      expect(updatedDestinationTable, equals(expectedWithInsertTable));
     });
   });
 }
