2c10a8d7d4945ef824ac5f3829933537bfdfcdd0
Anton Malofeev <arenukvern@gmail.com>
2023-02-05 04:26:40 +0200

    fix: visible errors

--- lib/pack_core/app/navigation_screen.dart
+++ lib/pack_core/app/navigation_screen.dart
@@ -1,12 +1,19 @@
 import 'dart:math' as math;
 
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:life_hooks/life_hooks.dart';
 import 'package:provider/provider.dart';
+import 'package:tables_syncer_excel_addin/pack_app/screens/home.dart';
+import 'package:tables_syncer_excel_addin/pack_app/screens/tables_list.dart';
+import 'package:tables_syncer_excel_addin/pack_app/screens/tables_syncs.dart';
+import 'package:tables_syncer_excel_addin/pack_app/screens/upsert_sync/tables_sync.dart';
 import 'package:tables_syncer_excel_addin/pack_app/widgets/widgets.dart';
+import 'package:tables_syncer_excel_addin/pack_core/app/info_screen.dart';
 import 'package:tables_syncer_excel_addin/pack_core/global_states/global_states.dart';
 import 'package:tables_syncer_excel_addin/pack_core/pack_core.dart';
+import 'package:tables_syncer_excel_addin/pack_settings/pack_settings.dart';
 import 'package:ts_design_core/ts_design_core.dart';
 
 part 'navigation_screen_state.dart';
@@ -30,20 +37,54 @@ class NavigationScreen extends HookWidget {
           final isHomeScreen = currentScreen == NavigationScreens.home;
           return Stack(
             children: [
-              NavigationView(
-                pane: Provider(
-                  create: (final context) => state,
-                  builder: (final context, final child) {
-                    return AppNavigationRail(
-                      panePaddingRequired: !isHomeScreen,
-                    );
-                  },
-                ),
-                appBar: isHomeScreen
-                    ? const NavigationAppBar(
-                        automaticallyImplyLeading: false,
-                      )
-                    : null,
+              Row(
+                children: [
+                  Provider(
+                    create: (final context) => state,
+                    builder: (final context, final child) {
+                      return AppNavigationRail(
+                        panePaddingRequired: !isHomeScreen,
+                      );
+                    },
+                  ),
+                  ValueListenableBuilder(
+                    valueListenable: state.currentScreen,
+                    builder: (final context, final currentScreen, final child) {
+                      return LazyIndexedStack(
+                        index: currentScreen.index,
+                        children: NavigationScreens.values.map(
+                          (final screen) {
+                            switch (screen) {
+                              case NavigationScreens.home:
+                                return const HomeScreen();
+                              case NavigationScreens.createSync:
+                                return const TablesSyncScreen();
+                              case NavigationScreens.info:
+                                return const InfoScreen();
+                              case NavigationScreens.profile:
+                                return ProfileScreen(
+                                  actions: [
+                                    SignedOutAction((final context) {
+                                      state.diDto.routerController.toSignIn();
+                                    }),
+                                  ],
+                                  providers: GlobalStateNotifiers
+                                          .getFirebaseIntializer()
+                                      .providers,
+                                );
+                              case NavigationScreens.settings:
+                                return const SettingsScreen();
+                              case NavigationScreens.syncs:
+                                return const TablesSyncsListScreen();
+                              case NavigationScreens.tables:
+                                return const TablesListScreen();
+                            }
+                          },
+                        ).toList(),
+                      );
+                    },
+                  ),
+                ],
               ),
               if (isHomeScreen)
                 Positioned(
@@ -89,51 +130,45 @@ class AppNavigationRail extends StatelessWidget {
       valueListenable: state.currentScreen,
       builder: (final context, final currentScreen, final child) {
         return NavigationRail(
-          destinations: const [
-            NavigationRailDestination(
-              icon: Icon(Icons.home),
-              label: Text('Home'),
-              // body: HomeScreen(),
-            ),
-            NavigationRailDestination(
-              icon: Icon(Icons.add),
-              label: Text('Create sync'),
-              // body: TablesSyncScreen(),
-            ),
-            NavigationRailDestination(
-              icon: Icon(Icons.sync),
-              label: Text('Syncs'),
-              // body: TablesSyncsListScreen(),
-            ),
-            NavigationRailDestination(
-              icon: Icon(Icons.table_view_rounded),
-              label: Text('Tables'),
-              // body: TablesListScreen(),
-            ),
-            NavigationRailDestination(
-              icon: Icon(Icons.settings),
-              label: Text('Settings'),
-              // body: SettingsScreen(),
-            ),
-            NavigationRailDestination(
-              icon: Icon(Icons.settings),
-              label: Text('Profile'),
-              // body: ProfileScreen(
-              //   actions: [
-              //     SignedOutAction((final context) {
-              //       state.diDto.routerController.toSignIn();
-              //     }),
-              //   ],
-              //   providers:
-              //       GlobalStateNotifiers.getFirebaseIntializer().providers,
-              // ),
-            ),
-            NavigationRailDestination(
-              icon: Icon(Icons.info),
-              label: Text('Info'),
-              // body: InfoScreen(),
-            ),
-          ],
+          destinations: NavigationScreens.values.map((final screen) {
+            switch (screen) {
+              case NavigationScreens.home:
+                return const NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                );
+              case NavigationScreens.createSync:
+                return const NavigationRailDestination(
+                  icon: Icon(Icons.add),
+                  label: Text('Create sync'),
+                );
+              case NavigationScreens.info:
+                return const NavigationRailDestination(
+                  icon: Icon(Icons.info),
+                  label: Text('Info'),
+                );
+              case NavigationScreens.profile:
+                return const NavigationRailDestination(
+                  icon: Icon(Icons.person),
+                  label: Text('Profile'),
+                );
+              case NavigationScreens.settings:
+                return const NavigationRailDestination(
+                  icon: Icon(Icons.settings),
+                  label: Text('Settings'),
+                );
+              case NavigationScreens.syncs:
+                return const NavigationRailDestination(
+                  icon: Icon(Icons.sync),
+                  label: Text('Syncs'),
+                );
+              case NavigationScreens.tables:
+                return const NavigationRailDestination(
+                  icon: Icon(Icons.table_view_rounded),
+                  label: Text('Tables'),
+                );
+            }
+          }).toList(),
           onDestinationSelected: (final index) => state.onNavigationChanged(
             NavigationScreens.values
                 .firstWhere((final value) => value.index == index),
@@ -141,6 +176,60 @@ class AppNavigationRail extends StatelessWidget {
           selectedIndex: currentScreen.index,
         );
       },
+    );
+  }
+}
+
+/// A lazy-loading [IndexedStack] that loads [children] accordingly.
+class LazyIndexedStack extends StatefulWidget {
+  const LazyIndexedStack({
+    super.key,
+    this.alignment = AlignmentDirectional.topStart,
+    this.textDirection,
+    this.sizing = StackFit.loose,
+    this.index = 0,
+    this.children = const [],
+  });
+
+  final AlignmentGeometry alignment;
+  final TextDirection? textDirection;
+  final StackFit sizing;
+  final int index;
+  final List<Widget> children;
+
+  @override
+  LazyIndexedStackState createState() => LazyIndexedStackState();
+}
+
+class LazyIndexedStackState extends State<LazyIndexedStack> {
+  late List<bool> _activated = _initializeActivatedList();
+
+  List<bool> _initializeActivatedList() => List<bool>.generate(
+        widget.children.length,
+        (final i) => i == widget.index,
+      );
+
+  @override
+  void didUpdateWidget(covariant final LazyIndexedStack oldWidget) {
+    if (oldWidget.children.length != widget.children.length) {
+      _activated = _initializeActivatedList();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    // Mark current index as active
+    _activated[widget.index] = true;
+    final children = List.generate(_activated.length, (final i) {
+      return _activated[i] ? widget.children[i] : const SizedBox.shrink();
+    });
+    return IndexedStack(
+      alignment: widget.alignment,
+      sizing: widget.sizing,
+      textDirection: widget.textDirection,
+      index: widget.index,
+      children: children,
     );
   }
 }
